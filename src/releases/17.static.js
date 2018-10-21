6ceac9f7da19f4f9474caab9f06b9d16427b3869
racolagexxx <racolagexxx@protonmail.com>
2018-10-21 23:55:20 +0200

    added release 17 stats

--- src/releases/17.static.js
+++ src/releases/17.static.js
@@ -1,11 +1,29 @@
 const { component } = require('../base.static')
 const ReleaseCoverPopUp = require('../components/ReleaseCoverPopUp.static')
 const ReleaseAnalyticsPopUp = require('../components/ReleaseAnalyticsPopUp.static')
+const MediaPopUp = require('../components/MediaPopUp.static')
 
 module.exports = component(__filename, () => {
 
   const renderHTML = function(attrs) {
     return `
+
+      ${MediaPopUp.renderHTML({
+        image: { srcUrl: 'releases/17/screengrab2.jpg' }
+      })}
+
+      ${MediaPopUp.renderHTML({
+        image: { srcUrl: 'releases/17/screengrab1.jpg' }
+      })}
+
+      ${ReleaseAnalyticsPopUp.renderHTML({
+          date: '21/10/2018',
+          table: [
+            ['Videos Uploaded', '10'],
+            ['Total Plays', '16 000']  
+          ]
+        })}
+
       ${ReleaseCoverPopUp.renderHTML(attrs)}
     `
   }

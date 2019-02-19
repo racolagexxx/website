16cd3575edf263f2cd1d6f9cad900dff763a374e
racolagexxx <racolagexxx@protonmail.com>
2019-02-19 20:24:47 +0100

    added stats for release 20

--- src/components/releases/20.index.html.js
+++ src/components/releases/20.index.html.js
@@ -1,10 +1,30 @@
 const declareStaticComponent = require('~/declareStaticComponent')
 const ReleaseCoverPopUp = require('~/components/ReleaseCoverPopUp.static')
+const MediaPopUp = require('~/components/MediaPopUp.static')
+const ReleaseAnalyticsPopUp = require('~/components/ReleaseAnalyticsPopUp.static')
+
 
 module.exports = declareStaticComponent(__filename, () => {
 
   const renderHTML = function(attrs) {
     return `
+
+      ${MediaPopUp.renderHTML({
+        image: { srcUrl: 'releases/20/screengrab1.jpg' }
+      })}
+
+      ${MediaPopUp.renderHTML({
+        image: { srcUrl: 'releases/20/screengrab2.jpg' }
+      })}
+
+      ${ReleaseAnalyticsPopUp.renderHTML({
+        date: '19/02/2018',
+        table: [
+          ['Videos Uploaded', '10'],
+          ['Total Plays', '37050']  
+        ]
+      })}
+
       ${ReleaseCoverPopUp.renderHTML(attrs)}
     `
   }

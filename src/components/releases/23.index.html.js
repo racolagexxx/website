191dbfed9ae28ae13bf22e8c4aac89c7a4588cc7
racolagexxx <racolagexxx@protonmail.com>
2019-03-01 14:33:14 +0100

    release 23

---
+++ src/components/releases/23.index.html.js
@@ -0,0 +1,28 @@
+const declareStaticComponent = require('~/declareStaticComponent')
+const ReleaseCoverPopUp = require('~/components/ReleaseCoverPopUp.static')
+const PopUp = require('~/components/PopUp.static')
+const MediaPopUp = require('~/components/MediaPopUp.static')
+
+module.exports = declareStaticComponent(__filename, () => {
+
+  const renderHTML = function(attrs) {
+    return `
+
+      ${PopUp.renderHTML(
+        { variant: 'popup2' }, `
+        <p>
+          Part 3/4 from <b>ivvill - cutyou</b> the first ever EP released on racolage.xxx. 
+        </p>
+        <p>
+          To celebrate, we unleash our full spam powers on several porn streaming sites and by sending 1 000 000 spam emails.
+        </p>
+      `)}
+
+      ${ReleaseCoverPopUp.renderHTML(attrs)}
+    `
+  }
+
+  const renderCSS = () => {}
+
+  return { renderHTML, renderCSS }
+})
